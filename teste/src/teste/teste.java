7700faa159d9aa3ecff174d7f3d772c95ab303c1
Carlos Eduardo Araujo <150200767+caduaraujjo@users.noreply.github.com>
2025-08-28 20:32:04 -0300

    Update teste.java

--- teste/src/teste/teste.java
+++ teste/src/teste/teste.java
@@ -1,10 +1,12 @@
 package teste;
 
-public class teste {
-
+public class Teste {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num1 = 3, num2 = 5, soma;
+		
+		soma = num1 + num2;
+		
 	}
-
+	
 }
